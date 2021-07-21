64d7d30d3c68a2d9a19dc4a387d8d6155ac666e5
QsVlEANN <evgeniy.radchin@stud.onu.edu.ua>
2021-07-21 14:46:22 +0300

    1 - test AnswerDAO

--- src/main/java/ua/netcracker/netcrackerquizb/model/impl/AnswerImpl.java
+++ src/main/java/ua/netcracker/netcrackerquizb/model/impl/AnswerImpl.java
@@ -2,10 +2,14 @@ package ua.netcracker.netcrackerquizb.model.impl;
 
 import ua.netcracker.netcrackerquizb.model.Answer;
 
+import java.math.BigInteger;
+
 public class AnswerImpl implements Answer {
 
+    private BigInteger id;
     private String value;
-    private Boolean answer;
+    private Boolean answer = false;
+    private BigInteger questionId;
 
     @Override
     public String getValue() {
@@ -13,20 +17,38 @@ public class AnswerImpl implements Answer {
     }
 
     @Override
-    public Boolean isTrue() {
-        return false;
+    public BigInteger getId() {
+        return id;
     }
 
-    public void setValue(String value) {
-        this.value = value;
-    }
-
+    @Override
     public Boolean getAnswer() {
         return answer;
     }
 
+    @Override
+    public BigInteger getQuestionId() {
+        return null;
+    }
+
+    @Override
+    public void setId(BigInteger id) {
+        this.id = id;
+    }
+
+    @Override
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
     public void setAnswer(Boolean answer) {
         this.answer = answer;
+    }
+
+    @Override
+    public void setQuestionId(BigInteger questionId) {
+        this.questionId = questionId;
     }
 
 }
