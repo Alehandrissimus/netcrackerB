c0df97974b46cc90ef4c2d5dc2a2ffee24e88268
QsVlEANN <evgeniy.radchin@stud.onu.edu.ua>
2021-07-26 22:10:32 +0300

    6 - edit DAOLogicException

--- src/main/java/ua/netcracker/netcrackerquizb/exception/AnswerDoesNotExistException.java
+++ src/main/java/ua/netcracker/netcrackerquizb/exception/AnswerDoesNotExistException.java
@@ -1,6 +1,14 @@
 package ua.netcracker.netcrackerquizb.exception;
 
 public class AnswerDoesNotExistException extends Exception {
+
+    public AnswerDoesNotExistException (String errorMessage) {
+        super(errorMessage);
+    }
+    public AnswerDoesNotExistException (String errorMessage, Throwable error) {
+        super(errorMessage, error);
+    }
+
     @Override
     public String toString() {
         return "Answer does not exist!\n" + super.toString();
