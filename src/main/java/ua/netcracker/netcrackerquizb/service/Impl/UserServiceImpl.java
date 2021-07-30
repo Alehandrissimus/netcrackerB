1a01f0c3d8ab5f5605c9eb24aed688aa7713c73e
Maksym Bataiev <max.bataiev@gmail.com>
2021-07-30 16:16:59 +0300

    user service

--- src/main/java/ua/netcracker/netcrackerquizb/service/Impl/UserServiceImpl.java
+++ src/main/java/ua/netcracker/netcrackerquizb/service/Impl/UserServiceImpl.java
@@ -1,13 +1,20 @@
 package ua.netcracker.netcrackerquizb.service.Impl;
 
 import static ua.netcracker.netcrackerquizb.exception.MessagesForException.DAO_LOGIC_EXCEPTION;
+import static ua.netcracker.netcrackerquizb.exception.MessagesForException.EMAIL_ERROR;
+import static ua.netcracker.netcrackerquizb.exception.MessagesForException.INVALID_USERS_EMAIL;
+import static ua.netcracker.netcrackerquizb.exception.MessagesForException.USER_NOT_FOUND_EXCEPTION;
 
 import java.math.BigInteger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ua.netcracker.netcrackerquizb.dao.UserDAO;
 import ua.netcracker.netcrackerquizb.exception.DAOLogicException;
+import ua.netcracker.netcrackerquizb.exception.MailException;
+import ua.netcracker.netcrackerquizb.exception.MessagesForException;
 import ua.netcracker.netcrackerquizb.exception.UserDoesNotConfirmedEmailException;
 import ua.netcracker.netcrackerquizb.exception.UserDoesNotExistException;
 import ua.netcracker.netcrackerquizb.exception.UserException;
@@ -22,12 +29,16 @@ public class UserServiceImpl implements UserService {
 
   @Autowired
   private UserDAO userDAO;
+  @Autowired
+  private MailSenderServiceImpl mailSenderService;
 
 
   @Override
   public BigInteger buildNewUser(String email, String password, String firstName, String lastName)
       throws UserException, DAOLogicException {
     try {
+
+      validateNewUser(email, password, firstName, lastName);
 
       if (userDAO.getUserByEmail(email) != null) {
         throw new UserException("User already exist");
@@ -65,12 +76,38 @@ public class UserServiceImpl implements UserService {
   }
 
   @Override
-  public User recoverPassword(User user) {
-    return null;
+  public void recoverPassword(User user) throws UserException, DAOLogicException, MailException {
+    try {
+      if (userDAO.getAuthorizeUser(user.getEmail(), user.getPassword()) != null) {
+        mailSenderService.generateNewPassword(user.getEmail());
+      }
+    } catch (UserDoesNotExistException | UserDoesNotConfirmedEmailException e) {
+      log.error(MessagesForException.USER_NOT_FOUND_EXCEPTION + e.getMessage());
+      throw new UserException(USER_NOT_FOUND_EXCEPTION, e);
+    } catch (DAOLogicException e) {
+      log.error(DAO_LOGIC_EXCEPTION + e.getMessage());
+      throw new DAOLogicException(DAO_LOGIC_EXCEPTION, e);
+    } catch (MailException e) {
+      log.error(MessagesForException.EMAIL_ERROR + e.getMessage());
+      throw new MailException(EMAIL_ERROR, e);
+    }
   }
 
   @Override
-  public void validateNewUser(User user) {
+  public void validateNewUser(String email, String password, String firstName, String lastName)
+      throws UserException {
+
+    Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+    Matcher matcher = pattern.matcher(email);
+
+    if (!matcher.find()) {
+      log.error(MessagesForException.INVALID_USERS_EMAIL);
+      throw new UserException(INVALID_USERS_EMAIL);
+    }
+
 
   }
+
+
 }
+
--- src/main/java/ua/netcracker/netcrackerquizb/service/UserService.java
+++ src/main/java/ua/netcracker/netcrackerquizb/service/UserService.java
@@ -2,16 +2,22 @@ package ua.netcracker.netcrackerquizb.service;
 
 import java.math.BigInteger;
 import ua.netcracker.netcrackerquizb.exception.DAOLogicException;
+import ua.netcracker.netcrackerquizb.exception.MailException;
 import ua.netcracker.netcrackerquizb.exception.UserException;
 import ua.netcracker.netcrackerquizb.model.User;
 
 public interface UserService {
-    BigInteger buildNewUser(String email, String password, String name, String surname)
-        throws UserException, DAOLogicException;
 
-    User authorize(User user) throws DAOLogicException, UserException;
+  String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" +
+          "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
 
-    User recoverPassword(User user);
+  BigInteger buildNewUser(String email, String password, String name, String surname)
+      throws UserException, DAOLogicException;
 
-    void validateNewUser(User user);
+  User authorize(User user) throws DAOLogicException, UserException;
+
+  void recoverPassword(User user) throws UserException, DAOLogicException, MailException;
+
+  void validateNewUser(String email, String password, String firstName, String lastName)
+      throws UserException;
 }
