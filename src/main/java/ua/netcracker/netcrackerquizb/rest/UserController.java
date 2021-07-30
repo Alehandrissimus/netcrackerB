3e0f195521b65ab42c56c36f4b6c026d0a8f35df
Alehandrissimus <buryachenko10@gmail.com>
2021-07-30 21:56:08 +0300

    Move UserController

--- src/main/java/ua/netcracker/netcrackerquizb/rest/UserController.java
+++ src/main/java/ua/netcracker/netcrackerquizb/rest/UserController.java
@@ -1,5 +1,116 @@
 package ua.netcracker.netcrackerquizb.rest;
 
-public class UserController {
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ua.netcracker.netcrackerquizb.exception.DAOLogicException;
+import ua.netcracker.netcrackerquizb.exception.MailException;
+import ua.netcracker.netcrackerquizb.exception.MessagesForException;
+import ua.netcracker.netcrackerquizb.exception.UserException;
+import ua.netcracker.netcrackerquizb.model.Quiz;
+import ua.netcracker.netcrackerquizb.model.User;
+import ua.netcracker.netcrackerquizb.model.impl.UserImpl;
+import ua.netcracker.netcrackerquizb.service.MailSenderService;
+import ua.netcracker.netcrackerquizb.service.UserService;
+import ua.netcracker.netcrackerquizb.util.RegexPatterns;
 
+import java.math.BigInteger;
+import java.util.List;
+
+@RestController
+public class UserController implements RegexPatterns {
+
+    private static final Logger log = Logger.getLogger(UserController.class);
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private MailSenderService mailSenderService;
+
+    @PostMapping("/register")
+    public void createUser(String email, String password, String name, String surname) {
+        try {
+            userService.validateNewUser(email, password, name, surname);
+            BigInteger userId = userService.buildNewUser(email, password, name, surname);
+
+            User user = new UserImpl.UserBuilder()
+                    .setId(userId)
+                    .setEmail(email)
+                    .build();
+
+            mailSenderService.sendEmail(user);
+        } catch (DAOLogicException e) {
+            log.error("a");
+        } catch (UserException e) {
+            log.error("a");
+        } catch (MailException e) {
+            log.error("a");
+        }
+    }
+
+    //return user?
+    @PostMapping("/login")
+    public void tryToAuthorize(String email, String password) {
+        try {
+            if(!email.matches(mailPattern)) {
+                throw new UserException(MessagesForException.INVALID_EMAIL);
+            }
+            if(!password.matches(passPattern)) {
+                throw new UserException(MessagesForException.INVALID_PASSWORD);
+            }
+            User user = new UserImpl.UserBuilder()
+                    .setEmail(email)
+                    .setPassword(password)
+                    .build();
+            User receivedUser = userService.authorize(user);
+        } catch (DAOLogicException e) {
+            log.error("a");
+        } catch (UserException e) {
+            log.error("a");
+        }
+    }
+
+    public void deleteUser(User user) {
+
+    }
+
+    public void editUser(User user) {
+
+    }
+
+    @PostMapping("/recover")
+    public void recoverPassword(User user) {
+        try {
+            userService.recoverPassword(user);
+        } catch (DAOLogicException e) {
+            log.error("a");
+        } catch (MailException e) {
+            log.error("a");
+        } catch (UserException e) {
+            log.error("a");
+        }
+    }
+
+
+    public void updatePassword() {
+
+    }
+
+    public void registrationConfirm(User user) {
+
+    }
+
+    public void confirmEmail(String confirmationCode) {
+
+    }
+
+    public User getUser(BigInteger userId) {
+        return null;
+    }
+
+    public List<Quiz> getAccomplishedQuizes(BigInteger userId) {
+        return null;
+    }
 }
